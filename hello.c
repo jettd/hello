027e185efad11628829c8e975f420ab1a2cec137
Jett <jettdurante54@icloud.com>
2019-08-29 12:39:18 -0500

    Create hello.c
    now prints name and major

---
+++ hello.c
@@ -0,0 +1,11 @@
+/**hello world with my name instead
+  *8/29/2019
+  *CSCE 155E
+  */
+
+int main (int argc, char **argv) {
+  printf("Jett Durante\n");
++
+  printf("Undeclared\n");
+  return 0
+}
